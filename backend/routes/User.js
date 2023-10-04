d1712f6b8d5638ad2cc7b156d631145e05070bcb
Umair <73277454+Proxmair@users.noreply.github.com>
2023-10-04 06:04:02 -0700

    Update User.js

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -8,6 +8,6 @@ export const userRouter=express.Router();
 userRouter.route("/login").post(login);
 userRouter.route("/logout").get(logout);
 userRouter.route("/user").get(getUser);
-userRouter.route("/me").get(isAuthenticated, myProfile);
+userRouter.route("/me").get(myProfile);
 userRouter.route("/update").post(isAuthenticated,updateProfile)
 userRouter.route("/message").post(SendMessage);
